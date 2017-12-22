49adbffd530bb25a2cefb61f4ac4cdd5ceb35167
fengzhizi715 <fengzhizi715@126.com>
2017-12-23 02:08:56 +0800

    update build.gradle and Request

--- core/src/main/java/com/cv4j/netdiscovery/core/http/Request.java
+++ core/src/main/java/com/cv4j/netdiscovery/core/http/Request.java
@@ -1,6 +1,9 @@
 package com.cv4j.netdiscovery.core.http;
 
+import com.safframework.tony.common.collection.NoEmptyHashMap;
 import lombok.Getter;
+
+import java.util.Map;
 
 /**
  * Created by tony on 2017/12/23.
@@ -12,8 +15,16 @@ public class Request {
 
     private String method;
 
+    private Map<String,String> header = new NoEmptyHashMap<>();
+
     public Request(String url) {
 
         this.url = url;
+    }
+
+    public Request header(String name,String value) {
+
+        header.put(name,value);
+        return this;
     }
 }
