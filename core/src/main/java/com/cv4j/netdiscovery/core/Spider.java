56de33dbce13344c4ebdac5a5cb62c0238dcc325
fengzhizi715 <fengzhizi715@126.com>
2018-01-01 15:02:13 +0800

    update Spider

--- core/src/main/java/com/cv4j/netdiscovery/core/Spider.java
+++ core/src/main/java/com/cv4j/netdiscovery/core/Spider.java
@@ -7,13 +7,16 @@ import com.cv4j.netdiscovery.core.parser.Parser;
 import com.cv4j.netdiscovery.core.pipeline.Pipeline;
 import com.cv4j.netdiscovery.core.queue.DefaultQueue;
 import com.cv4j.netdiscovery.core.queue.Queue;
+import com.cv4j.netdiscovery.core.queue.RedisQueue;
 import com.safframework.tony.common.utils.Preconditions;
+import com.safframework.tony.common.utils.StringUtils;
 import io.reactivex.functions.Consumer;
 import io.reactivex.functions.Function;
 import io.reactivex.schedulers.Schedulers;
 import io.vertx.reactivex.ext.web.client.HttpResponse;
 import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
+import redis.clients.jedis.JedisPool;
 
 import java.util.LinkedHashSet;
 import java.util.Set;
@@ -96,78 +99,88 @@ public class Spider {
 
     public void run() {
 
-        Request request = queue.poll(name);
+        while (true) {
 
-        if (request!=null) {
+            Request request = queue.poll(name);
 
-            VertxClient client = new VertxClient(request);
-            client.get()
-                    .observeOn(Schedulers.io())
-                    .map(new Function<HttpResponse<String>, Page>() {
+            if (request!=null) {
 
-                        @Override
-                        public Page apply(HttpResponse<String> stringHttpResponse) throws Exception {
+                VertxClient client = new VertxClient(request);
+                client.get()
+                        .observeOn(Schedulers.io())
+                        .map(new Function<HttpResponse<String>, Page>() {
 
-                            String html = stringHttpResponse.body();
+                            @Override
+                            public Page apply(HttpResponse<String> stringHttpResponse) throws Exception {
 
-                            Page page = new Page();
-                            page.setHtml(html);
-                            page.setRequest(request);
-                            page.setUrl(request.getUrl());
-                            page.setStatusCode(stringHttpResponse.statusCode());
+                                String html = stringHttpResponse.body();
 
-                            return page;
-                        }
-                    })
-                    .map(new Function<Page, Page>() {
+                                Page page = new Page();
+                                page.setHtml(html);
+                                page.setRequest(request);
+                                page.setUrl(request.getUrl());
+                                page.setStatusCode(stringHttpResponse.statusCode());
 
-                        @Override
-                        public Page apply(Page page) throws Exception {
-
-                            if (parser!=null) {
-
-                                parser.process(page);
+                                return page;
                             }
+                        })
+                        .map(new Function<Page, Page>() {
 
-                            return page;
-                        }
-                    })
-                    .map(new Function<Page, Page>() {
+                            @Override
+                            public Page apply(Page page) throws Exception {
 
-                        @Override
-                        public Page apply(Page page) throws Exception {
+                                if (parser!=null) {
 
-                            if (Preconditions.isNotBlank(pipelines)){
-
-                                for (Pipeline pipeline:pipelines) {
-
-                                    pipeline.process(page.getResultItems());
+                                    parser.process(page);
                                 }
+
+                                return page;
                             }
+                        })
+                        .map(new Function<Page, Page>() {
 
-                            return page;
-                        }
-                    })
-                    .subscribe(new Consumer<Page>() {
+                            @Override
+                            public Page apply(Page page) throws Exception {
 
-                        @Override
-                        public void accept(Page page) throws Exception {
+                                if (Preconditions.isNotBlank(pipelines)){
 
-                        }
-                    }, new Consumer<Throwable>() {
-                        @Override
-                        public void accept(Throwable throwable) throws Exception {
+                                    for (Pipeline pipeline:pipelines) {
 
-                            log.error(throwable.getMessage());
-                        }
-                    });
+                                        pipeline.process(page.getResultItems());
+                                    }
+                                }
+
+                                return page;
+                            }
+                        })
+                        .subscribe(new Consumer<Page>() {
+
+                            @Override
+                            public void accept(Page page) throws Exception {
+
+                                log.info(StringUtils.printObject(page));
+                            }
+                        }, new Consumer<Throwable>() {
+                            @Override
+                            public void accept(Throwable throwable) throws Exception {
+
+                                log.error(throwable.getMessage());
+                            }
+                        });
+            } else {
+
+                break;
+            }
         }
     }
 
     public static void main(String[] args) {
 
-        Spider.create()
-                .url("http://www.163.com/")
+        JedisPool pool = new JedisPool("127.0.0.1",6379);
+
+        Spider.create(new RedisQueue(pool))
+                .request(new Request("http://www.163.com/"))
+                .request(new Request("https://www.baidu.com/"))
                 .run();
     }
 }
