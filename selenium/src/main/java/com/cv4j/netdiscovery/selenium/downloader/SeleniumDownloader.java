223e3816df2333c1030858412ff79093a705d3a1
fengzhizi715 <fengzhizi715@126.com>
2018-01-28 01:13:36 +0800

    add SeleniumDownloader

---
+++ selenium/src/main/java/com/cv4j/netdiscovery/selenium/downloader/SeleniumDownloader.java
@@ -0,0 +1,55 @@
+package com.cv4j.netdiscovery.selenium.downloader;
+
+import com.cv4j.netdiscovery.core.domain.Request;
+import com.cv4j.netdiscovery.core.domain.Response;
+import com.cv4j.netdiscovery.core.downloader.Downloader;
+import io.reactivex.Maybe;
+import io.reactivex.MaybeEmitter;
+import io.reactivex.MaybeOnSubscribe;
+import io.reactivex.functions.Function;
+import org.openqa.selenium.WebDriver;
+
+/**
+ * Created by tony on 2018/1/28.
+ */
+public class SeleniumDownloader implements Downloader {
+
+    WebDriver webDriver;
+
+    public SeleniumDownloader(WebDriver webDriver) {
+
+        this.webDriver = webDriver;
+    }
+
+
+    @Override
+    public Maybe<Response> download(Request request) {
+
+        return Maybe.create(new MaybeOnSubscribe<String>(){
+
+            @Override
+            public void subscribe(MaybeEmitter emitter) throws Exception {
+
+                emitter.onSuccess(webDriver.getPageSource());
+            }
+        }).map(new Function<String, Response>() {
+
+            @Override
+            public Response apply(String html) throws Exception {
+
+                Response response = new Response();
+                response.setContent(html);
+                response.setStatusCode(200);
+                return response;
+            }
+        });
+    }
+
+    @Override
+    public void close() {
+
+        if (webDriver!=null) {
+            webDriver.close();
+        }
+    }
+}
