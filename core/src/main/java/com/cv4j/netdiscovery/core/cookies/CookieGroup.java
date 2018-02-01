19ab86ac2b960843d979b58f24309261e41d720a
fengzhizi715 <fengzhizi715@126.com>
2018-02-01 14:55:13 +0800

    update CookieGroup

--- core/src/main/java/com/cv4j/netdiscovery/core/cookies/CookieGroup.java
+++ core/src/main/java/com/cv4j/netdiscovery/core/cookies/CookieGroup.java
@@ -1,6 +1,6 @@
 package com.cv4j.netdiscovery.core.cookies;
 
-import lombok.Data;
+import lombok.Getter;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -8,7 +8,7 @@ import java.util.Map;
 /**
  * Created by tony on 2018/2/1.
  */
-@Data
+@Getter
 public class CookieGroup {
 
     private String domain;
@@ -18,4 +18,30 @@ public class CookieGroup {
         this.domain = domain;
         this.cookies = new HashMap<>();
     }
+
+    public void putCookie(String key, String value) {
+        this.getCookies().put(key.trim(), value.trim());
+    }
+
+    public void removeCookie(String key) {
+        this.getCookies().remove(key);
+    }
+
+    public String getCookieString() {
+
+        StringBuilder buffer = new StringBuilder();
+
+        cookies.forEach((key, value) -> buffer.append(key).append("=").append(value).append("; "));
+
+        return buffer.length() > 2 ? buffer.substring(0, buffer.length() - 2) : buffer.toString();
+    }
+
+    @Override
+    public String toString() {
+        return "CookieGroup{" +
+                "domain='" + domain + '\'' +
+                ", cookies=" + cookies +
+                '}';
+    }
+
 }
