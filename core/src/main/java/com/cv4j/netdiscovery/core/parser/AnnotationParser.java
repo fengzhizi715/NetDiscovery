ed04bc24f6cfb44db7299f707312cc5145f00e51
fengzhizi715 <fengzhizi715@126.com>
2018-02-04 20:46:01 +0800

    update AnnotationParser

--- core/src/main/java/com/cv4j/netdiscovery/core/parser/AnnotationParser.java
+++ core/src/main/java/com/cv4j/netdiscovery/core/parser/AnnotationParser.java
@@ -3,6 +3,7 @@ package com.cv4j.netdiscovery.core.parser;
 import com.cv4j.netdiscovery.core.domain.Page;
 import com.cv4j.netdiscovery.core.domain.ResultItems;
 import com.cv4j.netdiscovery.core.parser.annotation.ExtractBy;
+import com.cv4j.netdiscovery.core.parser.annotation.ListFields;
 import com.safframework.tony.common.utils.Preconditions;
 import lombok.extern.slf4j.Slf4j;
 
@@ -13,7 +14,7 @@ import java.util.Arrays;
  * Created by tony on 2018/2/4.
  */
 @Slf4j
-public class AnnotationParser implements Parser{
+public class AnnotationParser implements Parser {
 
     @Override
     public void process(Page page) {
@@ -27,13 +28,19 @@ public class AnnotationParser implements Parser{
         if (Preconditions.isNotBlank(fields)) {
 
             Arrays.asList(fields)
-                    .forEach(field->{
+                    .forEach(field -> {
 
                         ExtractBy.XPath xpath = field.getAnnotation(ExtractBy.XPath.class);
 
-                        if (xpath!=null) {
+                        if (xpath != null) {
 
-                            resultItems.put(field.getName(),page.getHtml().xpath(xpath.value()));
+                            ListFields listField = field.getAnnotation(ListFields.class);
+
+                            if (listField == null) {
+                                resultItems.put(field.getName(), page.getHtml().xpath(xpath.value()));
+                            } else {
+                                resultItems.put(field.getName(), page.getHtml().xpath(xpath.value()).all());
+                            }
                         }
                     });
         }
