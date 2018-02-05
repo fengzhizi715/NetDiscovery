49e38396e2a05c9a35697a82ad62ec3badcfb76a
fengzhizi715 <fengzhizi715@126.com>
2018-02-05 22:28:00 +0800

    update readme.md

--- core/src/main/java/com/cv4j/netdiscovery/core/pipeline/debug/DebugPipeline.java
+++ core/src/main/java/com/cv4j/netdiscovery/core/pipeline/debug/DebugPipeline.java
@@ -1,6 +1,5 @@
 package com.cv4j.netdiscovery.core.pipeline.debug;
 
-import com.cv4j.netdiscovery.core.domain.Request;
 import com.cv4j.netdiscovery.core.domain.ResultItems;
 import com.cv4j.netdiscovery.core.pipeline.Pipeline;
 
