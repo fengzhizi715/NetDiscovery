945804b620e0da04fbfcf51b1a34b18138212cd8
fengzhizi715 <fengzhizi715@126.com>
2018-02-14 15:43:52 +0800

    update KafkaQueue

--- extra/src/main/java/com/cv4j/netdiscovery/extra/queue/kafka/KafkaQueue.java
+++ extra/src/main/java/com/cv4j/netdiscovery/extra/queue/kafka/KafkaQueue.java
@@ -7,8 +7,11 @@ import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.clients.producer.Callback;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.RecordMetadata;
+import org.apache.kafka.common.serialization.Serializer;
 
 import java.util.Arrays;
 import java.util.Properties;
@@ -29,10 +32,21 @@ public class KafkaQueue extends AbstractQueue {
         kafkaConsumer.subscribe(Arrays.asList(spiderName));
     }
 
+    public KafkaQueue(Properties producerProperties, Properties consumeProperties, Serializer<String> keySerializer, Serializer<Request> valueSerializer,String spiderName) {
+
+        producer = new KafkaProducer<String, Request>(producerProperties,keySerializer,valueSerializer);
+        kafkaConsumer = new KafkaConsumer<>(consumeProperties);
+        kafkaConsumer.subscribe(Arrays.asList(spiderName));
+    }
+
     @Override
     protected void pushWhenNoDuplicate(Request request) {
 
-        producer.send(new ProducerRecord<String, Request>(request.getSpiderName(), request));
+        try {
+            producer.send(new ProducerRecord<String, Request>(request.getSpiderName(), request));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
