f1ac6d0d663ee11541d2c7ae8c3b9771fd6e5d54
fengzhizi715 <fengzhizi715@126.com>
2018-02-17 17:46:51 +0800

    add CouchbasePipeline

---
+++ extra/src/main/java/com/cv4j/netdiscovery/extra/pipeline/CouchbasePipeline.java
@@ -0,0 +1,47 @@
+package com.cv4j.netdiscovery.extra.pipeline;
+
+import com.couchbase.client.java.Bucket;
+import com.couchbase.client.java.CouchbaseCluster;
+import com.couchbase.client.java.document.JsonDocument;
+import com.couchbase.client.java.document.json.JsonObject;
+import com.cv4j.netdiscovery.core.domain.ResultItems;
+import com.cv4j.netdiscovery.core.pipeline.Pipeline;
+
+import java.util.Map;
+
+/**
+ * Created by tony on 2018/2/17.
+ */
+public class CouchbasePipeline implements Pipeline{
+
+    private CouchbaseCluster cluster;
+    private Bucket bucket;
+    private String documentId;
+
+    public CouchbasePipeline(CouchbaseCluster cluster,Bucket bucket,String documentId){
+        this.cluster = cluster;
+        this.bucket = bucket;
+        this.documentId = documentId;
+    }
+
+    public CouchbasePipeline(CouchbaseCluster cluster,String bucketName,String documentId){
+        this.cluster = cluster;
+        this.bucket = cluster.openBucket(bucketName);
+        this.documentId = documentId;
+    }
+
+    @Override
+    public void process(ResultItems resultItems) {
+
+        JsonObject content = JsonObject.create();
+        for (Map.Entry<String, Object> entry : resultItems.getAll().entrySet()) {
+
+            content.put(entry.getKey(),entry.getValue());
+        }
+
+        bucket.upsert(JsonDocument.create(documentId, content));
+
+        // Close all buckets and disconnect
+        cluster.disconnect();
+    }
+}
