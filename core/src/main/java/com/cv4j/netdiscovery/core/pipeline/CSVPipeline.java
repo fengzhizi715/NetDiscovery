c7e1db6dfbb9c079b84d38cfaf149318709e2f5a
fengzhizi715 <fengzhizi715@126.com>
2018-02-23 00:31:49 +0800

    update CSVPipeline

--- core/src/main/java/com/cv4j/netdiscovery/core/pipeline/CSVPipeline.java
+++ core/src/main/java/com/cv4j/netdiscovery/core/pipeline/CSVPipeline.java
@@ -1,20 +1,62 @@
 package com.cv4j.netdiscovery.core.pipeline;
 
 import com.cv4j.netdiscovery.core.domain.ResultItems;
+import com.cv4j.netdiscovery.core.utils.Utils;
+import com.safframework.tony.common.utils.Preconditions;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
 
 /**
  * Created by tony on 2018/2/22.
  */
 public class CSVPipeline implements Pipeline{
 
-    String filePath;
+    private String filePath;
+    private String fileName;
+    private File csvFile = null;
 
-    public CSVPipeline(String filePath) {
+    public CSVPipeline(String filePath,String fileName) {
         this.filePath = filePath;
+        this.fileName = fileName;
+
+        csvFile = new File(filePath + fileName + ".csv");
+
+        if (!csvFile.exists()) {
+
+            File parent = csvFile.getParentFile();
+            if (parent != null && !parent.exists()) {
+                parent.mkdirs();
+            }
+
+            try {
+                csvFile.createNewFile();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
     @Override
     public void process(ResultItems resultItems) {
+        StringBuilder sb = new StringBuilder();
 
+        for (Map.Entry<String, Object> entry : resultItems.getAll().entrySet()) {
+
+            sb.append(entry.getValue()).append(",");
+        }
+
+        String[] ss = sb.toString().split(",");
+
+        if (Preconditions.isNotBlank(ss)) {
+
+            List<String> dataList = Arrays.asList(ss);
+            Utils.exportCsv(csvFile,dataList);
+        }
     }
+
+
 }
--- core/src/main/java/com/cv4j/netdiscovery/core/utils/Utils.java
+++ core/src/main/java/com/cv4j/netdiscovery/core/utils/Utils.java
@@ -2,10 +2,12 @@ package com.cv4j.netdiscovery.core.utils;
 
 import com.cv4j.proxy.domain.Proxy;
 import com.safframework.tony.common.utils.IOUtils;
+import com.safframework.tony.common.utils.Preconditions;
 
-import java.io.IOException;
+import java.io.*;
 import java.net.InetSocketAddress;
 import java.net.Socket;
+import java.util.List;
 
 /**
  * Created by tony on 2018/1/9.
@@ -33,5 +35,60 @@ public class Utils {
     public static boolean isTextType(String contentType) {
 
         return contentType!=null?contentType.startsWith("text"):false;
+    }
+
+    /**
+     * 导出
+     *
+     * @param file csv文件(路径+文件名)，csv文件不存在会自动创建
+     * @param dataList 数据
+     * @return
+     */
+    public static boolean exportCsv(File file, List<String> dataList){
+        boolean isSucess=false;
+
+        FileOutputStream out=null;
+        OutputStreamWriter osw=null;
+        BufferedWriter bw=null;
+        try {
+            out = new FileOutputStream(file);
+            osw = new OutputStreamWriter(out);
+            bw =new BufferedWriter(osw);
+            if(Preconditions.isNotBlank(dataList)){
+                for(String data : dataList){
+                    bw.append(data).append("\r");
+                }
+            }
+            isSucess=true;
+        } catch (Exception e) {
+            isSucess=false;
+        }finally{
+            if(bw!=null){
+                try {
+                    bw.close();
+                    bw=null;
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            if(osw!=null){
+                try {
+                    osw.close();
+                    osw=null;
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            if(out!=null){
+                try {
+                    out.close();
+                    out=null;
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        return isSucess;
     }
 }
