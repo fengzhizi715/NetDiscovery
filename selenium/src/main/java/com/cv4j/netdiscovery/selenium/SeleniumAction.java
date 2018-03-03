8ef062472bf6d902ffe7db9f68985ad00bb19a2d
fengzhizi715 <fengzhizi715@126.com>
2018-03-03 12:39:22 +0800

    add SeleniumAction

---
+++ selenium/src/main/java/com/cv4j/netdiscovery/selenium/SeleniumAction.java
@@ -0,0 +1,11 @@
+package com.cv4j.netdiscovery.selenium;
+
+import org.openqa.selenium.WebDriver;
+
+/**
+ * Created by tony on 2018/3/3.
+ */
+public interface SeleniumAction {
+
+    void execute(WebDriver driver);
+}
