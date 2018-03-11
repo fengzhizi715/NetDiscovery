e9e883856c926eb929b35519d5220223b73d344a
fengzhizi715 <fengzhizi715@126.com>
2018-03-11 13:39:18 +0800

    update Constant

--- core/src/main/java/com/cv4j/netdiscovery/core/config/Constant.java
+++ core/src/main/java/com/cv4j/netdiscovery/core/config/Constant.java
@@ -14,4 +14,6 @@ public class Constant {
     public static final String UTF_8 = "UTF-8";
 
     public static final int OK_STATUS_CODE = 200;
+
+    public static final String COOKIES_HEADER = "Set-Cookie";
 }
