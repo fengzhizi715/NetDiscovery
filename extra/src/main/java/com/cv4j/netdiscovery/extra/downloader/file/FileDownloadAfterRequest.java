d51e9d6a78a44316972e41dc778e481c7c16805a
fengzhizi715 <fengzhizi715@126.com>
2018-03-12 10:23:48 +0800

    add FileDownloadAfterRequest

---
+++ extra/src/main/java/com/cv4j/netdiscovery/extra/downloader/file/FileDownloadAfterRequest.java
@@ -0,0 +1,41 @@
+package com.cv4j.netdiscovery.extra.downloader.file;
+
+import com.cv4j.netdiscovery.core.config.Constant;
+import com.cv4j.netdiscovery.core.domain.Page;
+import com.cv4j.netdiscovery.core.domain.Request;
+import com.safframework.tony.common.utils.IOUtils;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
+ * Created by tony on 2018/3/12.
+ */
+public class FileDownloadAfterRequest implements Request.AfterRequest {
+
+    private String filePath;
+    private String fileName;
+
+    public FileDownloadAfterRequest(String filePath,String fileName) {
+        this.filePath = filePath;
+        this.fileName = fileName;
+    }
+
+    @Override
+    public void process(Page page) {
+
+        InputStream is = page.getResultItems().get(Constant.RESPONSE_RAW);
+        if (is!=null) {
+
+            try {
+                IOUtils.writeToFile(is,new File(filePath+fileName));
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+
+                IOUtils.closeQuietly(is);
+            }
+        }
+    }
+}
