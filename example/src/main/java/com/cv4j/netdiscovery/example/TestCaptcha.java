998fe5334abe4da540c8ce0f0f7e613cec34a7ee
fengzhizi715 <fengzhizi715@126.com>
2018-03-25 16:46:31 +0800

    update TestCaptcha

--- example/src/main/java/com/cv4j/netdiscovery/example/TestCaptcha.java
+++ example/src/main/java/com/cv4j/netdiscovery/example/TestCaptcha.java
@@ -9,8 +9,16 @@ public class TestCaptcha {
 
     public static void main(String[] args) {
 
+        System.out.println(Utils.getCaptcha("http://47.97.7.119/qianmou/images/captcha/1.png"));
+
+        System.out.println(Utils.getCaptcha("http://47.97.7.119/qianmou/images/captcha/2.png"));
+
         System.out.println(Utils.getCaptcha("http://47.97.7.119/qianmou/images/captcha/3.png"));
 
         System.out.println(Utils.getCaptcha("http://47.97.7.119/qianmou/images/captcha/4.png"));
+
+        System.out.println(Utils.getCaptcha("http://47.97.7.119/qianmou/images/captcha/5.png"));
+
+        System.out.println(Utils.getCaptcha("http://47.97.7.119/qianmou/images/captcha/6.png"));
     }
 }
