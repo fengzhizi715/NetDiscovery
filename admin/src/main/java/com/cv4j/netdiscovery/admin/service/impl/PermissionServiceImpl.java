433c6bc2de1840888ea6e910cc8db9653866a0d2
fengzhizi715 <fengzhizi715@126.com>
2018-04-06 16:26:56 +0800

    update PermissionServiceImpl

--- admin/src/main/java/com/cv4j/netdiscovery/admin/service/impl/PermissionServiceImpl.java
+++ admin/src/main/java/com/cv4j/netdiscovery/admin/service/impl/PermissionServiceImpl.java
@@ -3,6 +3,7 @@ package com.cv4j.netdiscovery.admin.service.impl;
 import com.cv4j.netdiscovery.admin.domain.Permission;
 import com.cv4j.netdiscovery.admin.mapper.PermissionMapper;
 import com.cv4j.netdiscovery.admin.service.PermissionService;
+import com.safframework.tony.common.utils.Preconditions;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -19,18 +20,23 @@ public class PermissionServiceImpl implements PermissionService {
 	public List<Permission> getMenus() {
 		List<Permission> result = new ArrayList<Permission>();
 		List<Permission> permissions = permissionMapper.selectPermissions();
-		for(Permission menu1 : permissions) {
-			if("0".equals(menu1.getParentId())){  //0代表一级菜单
-				List<Permission> subMenu = new ArrayList<>();
-				for(Permission menu2 : permissions) {
-					if(menu1.getPermissionId().equals(menu2.getParentId())) {
-						subMenu.add(menu2);
+
+		if (Preconditions.isNotBlank(permissions)) {
+
+			for(Permission menu1 : permissions) {
+				if("0".equals(menu1.getParentId())){  //0代表一级菜单
+					List<Permission> subMenu = new ArrayList<>();
+					for(Permission menu2 : permissions) {
+						if(menu1.getPermissionId().equals(menu2.getParentId())) {
+							subMenu.add(menu2);
+						}
 					}
+					menu1.setSubMenus(subMenu);
+					result.add(menu1);
 				}
-				menu1.setSubMenus(subMenu);
-				result.add(menu1);
 			}
 		}
+
 		return result;
 	}
 
