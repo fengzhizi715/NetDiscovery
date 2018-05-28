303632f7d2a8431c0feeeef7ef6b46207bd6fa74
fengzhizi715 <fengzhizi715@126.com>
2018-05-28 12:54:24 +0800

    add TestDSL.kt

---
+++ example/src/main/java/com/cv4j/netdiscovery/example/TestDSL.kt
@@ -0,0 +1,25 @@
+package com.cv4j.netdiscovery.example
+
+import com.cv4j.netdiscovery.core.pipeline.ConsolePipeline
+import com.cv4j.netdiscovery.dsl.spider
+
+
+/**
+ * Created by tony on 2018/5/27.
+ */
+object TestDSL {
+
+    @JvmStatic
+    fun main(args: Array<String>) {
+
+        spider {
+
+            name = "tony"
+
+            urls = listOf("http://www.163.com/","https://www.baidu.com/")
+
+            pipelines = hashSetOf(ConsolePipeline())
+        }
+    }
+
+}
