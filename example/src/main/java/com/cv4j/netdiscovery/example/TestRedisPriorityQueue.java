739e0610d6e40c45d73df391a7c7ba3a10872185
fengzhizi715 <fengzhizi715@126.com>
2018-06-19 21:35:24 +0800

    add TestRedisPriorityQueue

---
+++ example/src/main/java/com/cv4j/netdiscovery/example/TestRedisPriorityQueue.java
@@ -0,0 +1,22 @@
+package com.cv4j.netdiscovery.example;
+
+import com.cv4j.netdiscovery.core.Spider;
+import com.cv4j.netdiscovery.core.domain.Request;
+import com.cv4j.netdiscovery.extra.queue.redis.RedisPriorityQueue;
+
+/**
+ * Created by tony on 2018/6/19.
+ */
+public class TestRedisPriorityQueue {
+
+    public static void main(String[] args) {
+
+        Request request1 = new Request("http://www.163.com").priority(15);
+
+        Request request2 = new Request("https://www.jianshu.com/u/4f2c483c12d8").priority(10);
+
+        Spider.create(new RedisPriorityQueue("127.0.0.1")).name("tony2")
+                .request(request1,request2)
+                .run();
+    }
+}
