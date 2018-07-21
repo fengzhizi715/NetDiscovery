7d27fcac07da02417f7b860d01f6178a3475edfa
fengzhizi715 <fengzhizi715@126.com>
2018-07-21 15:04:23 +0800

    增加注释

--- example/src/main/java/com/cv4j/netdiscovery/example/TestFile.java
+++ example/src/main/java/com/cv4j/netdiscovery/example/TestFile.java
@@ -13,11 +13,11 @@ public class TestFile {
     public static void main(String[] args) {
 
         Request request = new Request("http://down-www.7down.net/pcdown/soft/K/kotlinpdf.rar");
-        request.afterRequest(new FileDownloadAfterRequest("test","test.rar"));
+        request.afterRequest(new FileDownloadAfterRequest("test","test.rar")); // 在使用FileDownloader时，可以使用AfterRequest或者Pipeline对文件进行保存等处理。这里使用FileDownloadAfterRequest
 
         Spider.create().name("tony")
                 .request(request)
-                .downloader(new FileDownloader())
+                .downloader(new FileDownloader()) // 文件的下载需要使用FileDownloader
                 .run();
     }
 }
