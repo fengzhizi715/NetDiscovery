3af93fb7b051cc9725d913d31ec22cb8188bb410
fengzhizi715 <fengzhizi715@126.com>
2018-09-15 14:17:28 +0800

    update WebDriver+Extension.kt

--- dsl/src/main/kotlin/com/cv4j/netdiscovery/dsl/WebDriver+Extension.kt
+++ dsl/src/main/kotlin/com/cv4j/netdiscovery/dsl/WebDriver+Extension.kt
@@ -7,17 +7,17 @@ import org.openqa.selenium.WebElement
 /**
  * Created by tony on 2018/9/15.
  */
-inline fun WebDriver.element(by: By, init: WebElement.() -> Unit) = findElement(by).init()
+fun WebDriver.element(by: By, init: WebElement.() -> Unit) = findElement(by).init()
 
-inline fun WebDriver.elementById(id: String, init: WebElement.() -> Unit) = findElement(By.id(id)).init()
+fun WebDriver.elementById(id: String, init: WebElement.() -> Unit) = findElement(By.id(id)).init()
 
-inline fun <T> WebDriver.elementByClass(className: String, init: WebElement.() -> T): T = findElement(By.className(className)).init()
+fun <T> WebDriver.elementByClass(className: String, init: WebElement.() -> T): T = findElement(By.className(className)).init()
 
-inline fun WebDriver.elementByName(name: String, init: WebElement.() -> Unit) = findElement(By.name(name)).init()
+fun WebDriver.elementByName(name: String, init: WebElement.() -> Unit) = findElement(By.name(name)).init()
 
-inline fun <T> WebDriver.elementByTag(tag: String, init: WebElement.() -> T): T = findElement(By.tagName(tag)).init()
+fun <T> WebDriver.elementByTag(tag: String, init: WebElement.() -> T): T = findElement(By.tagName(tag)).init()
 
-inline fun WebDriver.elementBySelector(selector: String, init: WebElement.() -> Unit) = findElement(By.cssSelector(selector)).init()
+fun WebDriver.elementBySelector(selector: String, init: WebElement.() -> Unit) = findElement(By.cssSelector(selector)).init()
 
-inline fun WebDriver.elementByXpath(xpath: String, init: WebElement.() -> Unit) = findElement(By.xpath(xpath)).init()
+fun WebDriver.elementByXpath(xpath: String, init: WebElement.() -> Unit) = findElement(By.xpath(xpath)).init()
 
