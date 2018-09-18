86f454b2024f036bcfb67d1d671fe257950c764e
fengzhizi715 <fengzhizi715@126.com>
2018-09-18 16:47:07 +0800

    add TestDSL4Request.kt

---
+++ example/src/main/kotlin/com/cv4j/netdiscovery/example/TestDSL4Request.kt
@@ -0,0 +1,39 @@
+package com.cv4j.netdiscovery.example
+
+import com.cv4j.netdiscovery.core.Spider
+import com.cv4j.netdiscovery.core.pipeline.debug.DebugPipeline
+import com.cv4j.netdiscovery.dsl.request
+import io.vertx.core.http.HttpMethod
+
+/**
+ * Created by tony on 2018/9/18.
+ */
+object TestDSL4Request {
+
+    @JvmStatic
+    fun main(args: Array<String>) {
+
+        val request = request {
+
+            url = "https://www.baidu.com/"
+
+            httpMethod = HttpMethod.GET
+
+            spiderName = "tony"
+
+            header {
+
+                "111" to "2222"
+                "333" to "44444"
+            }
+
+            extras {
+
+                "tt" to "qqq"
+            }
+        }
+
+        Spider.create().request(request).pipeline(DebugPipeline()).run()
+    }
+
+}
