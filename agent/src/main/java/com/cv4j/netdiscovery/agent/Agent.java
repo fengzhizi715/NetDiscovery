bc2351d9a0fa3819c4693c7da7d179f515c07dc0
fengzhizi715 <fengzhizi715@126.com>
2018-09-26 23:43:24 +0800

    add agent module

---
+++ agent/src/main/java/com/cv4j/netdiscovery/agent/Agent.java
@@ -0,0 +1,33 @@
+package com.cv4j.netdiscovery.agent;
+
+import com.sun.management.OperatingSystemMXBean;
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Vertx;
+import io.vertx.core.json.JsonObject;
+
+import java.lang.management.ManagementFactory;
+import java.util.UUID;
+
+public class Agent extends AbstractVerticle {
+
+  public static void main(String[] args) {
+    Vertx vertx = Vertx.vertx();
+    vertx.deployVerticle(new Agent());
+  }
+
+  @Override
+  public void start() {
+    OperatingSystemMXBean systemMBean = ManagementFactory.getPlatformMXBean(OperatingSystemMXBean.class);
+    String pid = UUID.randomUUID().toString();
+    vertx.createHttpServer()
+        .requestHandler(req -> {
+          JsonObject metrics = new JsonObject();
+          metrics.put("CPU", systemMBean.getProcessCpuLoad());
+          metrics.put("Mem", systemMBean.getTotalPhysicalMemorySize() - systemMBean.getFreePhysicalMemorySize());
+          req.response()
+            .putHeader("Content-Type", "application/json")
+            .end(new JsonObject().put(pid, metrics).encode());
+        })
+        .listen(8081);
+  }
+}
