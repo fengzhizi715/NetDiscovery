53daa6644a3560b8ab278704b61714a46938cc68
fengzhizi715 <fengzhizi715@126.com>
2018-11-26 11:04:19 +0800

    update DisruptorQueue

--- core/src/main/java/com/cv4j/netdiscovery/core/queue/disruptor/Consumer.java
+++ core/src/main/java/com/cv4j/netdiscovery/core/queue/disruptor/Consumer.java
@@ -1,23 +1,19 @@
 package com.cv4j.netdiscovery.core.queue.disruptor;
 
 import com.lmax.disruptor.WorkHandler;
+import lombok.extern.slf4j.Slf4j;
 
 import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * Created by tony on 2018/9/2.
  */
+@Slf4j
 public class Consumer implements WorkHandler<RequestEvent> {
-
-    private static AtomicInteger count = new AtomicInteger(0);
 
     @Override
     public void onEvent(RequestEvent requestEvent) throws Exception {
 
-        count.incrementAndGet();
-    }
-
-    public int getCount(){
-        return count.get();
+        log.info("consumer:" + Thread.currentThread().getName() + " requestEvent: value=" + requestEvent.toString());
     }
 }
--- core/src/main/java/com/cv4j/netdiscovery/core/queue/disruptor/DisruptorQueue.java
+++ core/src/main/java/com/cv4j/netdiscovery/core/queue/disruptor/DisruptorQueue.java
@@ -4,6 +4,7 @@ import com.cv4j.netdiscovery.core.domain.Request;
 import com.cv4j.netdiscovery.core.queue.AbstractQueue;
 import com.lmax.disruptor.*;
 import com.lmax.disruptor.dsl.ProducerType;
+import lombok.extern.slf4j.Slf4j;
 
 import java.util.concurrent.Executors;
 import java.util.concurrent.atomic.AtomicInteger;
@@ -11,16 +12,17 @@ import java.util.concurrent.atomic.AtomicInteger;
 /**
  * Created by tony on 2018/9/1.
  */
+@Slf4j
 public class DisruptorQueue extends AbstractQueue {
 
     private RingBuffer<RequestEvent> ringBuffer;
-    private Consumer consumer = null;
+
+    private Consumer[] consumers = new Consumer[2];
     private Producer producer = null;
+    private int ringBufferSize = 1024*1024; // RingBuffer 大小，必须是 2 的 N 次方；
     private AtomicInteger count = new AtomicInteger(0);
 
     public DisruptorQueue() {
-
-        int ringBufferSize = 1024*1024; // RingBuffer 大小，必须是 2 的 N 次方；
 
         //创建ringBuffer
         ringBuffer = RingBuffer.create(ProducerType.MULTI,
@@ -35,16 +37,17 @@ public class DisruptorQueue extends AbstractQueue {
 
         SequenceBarrier barriers = ringBuffer.newBarrier();
 
-        consumer = new Consumer();
+        for (int i = 0; i < consumers.length; i++) {
+            consumers[i] = new Consumer();
+        }
 
         WorkerPool<RequestEvent> workerPool =
                 new WorkerPool<RequestEvent>(ringBuffer,
                         barriers,
-                        new IntEventExceptionHandler(),
-                        consumer);
-
+                        new EventExceptionHandler(),
+                        consumers);
         ringBuffer.addGatingSequences(workerPool.getWorkerSequences());
-        workerPool.start(Executors.newSingleThreadExecutor());
+        workerPool.start(Executors.newFixedThreadPool(4));
 
         producer = new Producer(ringBuffer);
     }
@@ -53,7 +56,7 @@ public class DisruptorQueue extends AbstractQueue {
     protected void pushWhenNoDuplicate(Request request) {
 
         producer.pushData(request);
-
+        count.incrementAndGet();
         try {
             Thread.sleep(100);
         } catch (InterruptedException e) {
@@ -66,7 +69,7 @@ public class DisruptorQueue extends AbstractQueue {
 
         Request request = ringBuffer.get(ringBuffer.getCursor()-getTotalRequests(spiderName)+1).getRequest();
         ringBuffer.next();
-        count.incrementAndGet();
+
         return request;
     }
 
@@ -77,12 +80,24 @@ public class DisruptorQueue extends AbstractQueue {
 
     public int getTotalRequests(String spiderName) {
 
-        return consumer.getCount();
+        return count.get();
     }
 
-    static class IntEventExceptionHandler implements ExceptionHandler {
-        public void handleEventException(Throwable ex, long sequence, Object event) {}
-        public void handleOnStartException(Throwable ex) {}
-        public void handleOnShutdownException(Throwable ex) {}
+    static class EventExceptionHandler implements ExceptionHandler {
+
+        public void handleEventException(Throwable ex, long sequence, Object event) {
+
+            log.debug("handleEventException：" + ex);
+        }
+
+        public void handleOnStartException(Throwable ex) {
+
+            log.debug("handleOnStartException：" + ex);
+        }
+
+        public void handleOnShutdownException(Throwable ex) {
+
+            log.debug("handleOnShutdownException：" + ex);
+        }
     }
 }
