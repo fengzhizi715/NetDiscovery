3886c91dfdd3badf07bf2f3cad2e613be3aece29
fengzhizi715 <fengzhizi715@126.com>
2019-01-03 16:40:32 +0800

    update Pipeline

--- core/src/main/java/com/cv4j/netdiscovery/core/pipeline/Pipeline.java
+++ core/src/main/java/com/cv4j/netdiscovery/core/pipeline/Pipeline.java
@@ -18,11 +18,26 @@ public interface Pipeline {
     /**
      * 方便在 pipeline 中往队列中发起爬取任务
      * @param spider
+     * @param originalRequest 原始的request，新的request可以继承原始request的header信息
+     * @param url
+     */
+    default void push(Spider spider, Request originalRequest, String url) {
+
+        push(spider,spider.getQueue(),originalRequest,url);
+    }
+
+    /**
+     * 方便在 pipeline 中往队列中发起爬取任务
+     * @param spider
      * @param queue 使用的queue
      * @param originalRequest 原始的request，新的request可以继承原始request的header信息
      * @param url
      */
     default void push(Spider spider, Queue queue, Request originalRequest, String url) {
+
+        if (spider==null || queue==null || originalRequest==null || Preconditions.isBlank(url)) {
+            return;
+        }
 
         Request request = new Request(url);
         Map<String,String> header = originalRequest.getHeader(); // 从原始request中获取header
@@ -34,7 +49,7 @@ public interface Pipeline {
             });
         }
 
-        if (queue.isEmpty(originalRequest.getSpiderName())) {
+        if (queue.isEmpty(spider.getName())) { // queue为空时，需要重新启动爬虫
 
             queue.push(request);
             spider.run(); // 重启爬虫
