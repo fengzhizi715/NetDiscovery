feb98a810a8655a0517209ea9c970f28f4cf0689
fengzhizi715 <fengzhizi715@126.com>
2019-01-03 10:31:53 +0800

    add TestCookiePool

---
+++ example/src/main/java/com/cv4j/netdiscovery/example/TestCookiePool.java
@@ -0,0 +1,38 @@
+package com.cv4j.netdiscovery.example;
+
+import com.cv4j.netdiscovery.core.Spider;
+import com.cv4j.netdiscovery.core.SpiderEngine;
+import com.cv4j.netdiscovery.core.cookies.CookiePool;
+import com.cv4j.netdiscovery.core.domain.Request;
+import com.cv4j.netdiscovery.core.downloader.urlconnection.UrlConnectionDownloader;
+
+/**
+ * Created by tony on 2019-01-02.
+ */
+public class TestCookiePool {
+
+    public static void main(String[] args) {
+
+        SpiderEngine spiderEngine = SpiderEngine.create();
+
+        Request request = new Request("https://www.facebook.com").saveCookie(true);
+
+        Spider spider = Spider.create().name("tony").downloader(new UrlConnectionDownloader()).request(request);
+
+        spiderEngine.addSpider(spider).run();
+
+        CookiePool.getInsatance()
+                .getCookieGroup(request.getUrlParser().getHost())
+                .getCookies()
+                .forEach(httpCookie -> {
+                    System.out.println(httpCookie.getDomain());
+                    System.out.println(httpCookie.getName());
+                    System.out.println(httpCookie.getMaxAge());
+                    System.out.println(httpCookie.getPath());
+                    System.out.println(httpCookie.getSecure());
+                    System.out.println(httpCookie.getVersion());
+                    System.out.println(httpCookie.getValue());
+                    System.out.println("-------------------------");
+                });
+    }
+}
