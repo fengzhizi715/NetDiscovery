aa500f9bd3477217dff5ad2ede94fcb772fffbb9
fengzhizi715 <fengzhizi715@126.com>
2019-04-25 17:09:41 +0800

    add TestPushRequestToRunninSpider

---
+++ example/src/main/java/com/cv4j/netdiscovery/example/TestPushRequestToRunninSpider.java
@@ -0,0 +1,42 @@
+package com.cv4j.netdiscovery.example;
+
+import com.cv4j.netdiscovery.core.Spider;
+import com.cv4j.netdiscovery.core.domain.Request;
+import com.cv4j.netdiscovery.core.queue.disruptor.DisruptorQueue;
+
+import java.util.concurrent.CompletableFuture;
+
+/**
+ * Created by tony on 2019-04-25.
+ */
+public class TestPushRequestToRunninSpider {
+
+    public static void main(String[] args) {
+
+        Spider spider = Spider.create(new DisruptorQueue())
+                .name("tony")
+                .url("http://www.163.com");
+
+        CompletableFuture.runAsync(()->{
+            spider.run();
+        });
+
+        try {
+            Thread.sleep(2000L);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        spider.getQueue().pushToRunninSpider(new Request("https://www.baidu.com", "tony"),spider);
+
+        try {
+            Thread.sleep(2000L);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        spider.getQueue().pushToRunninSpider(new Request("https://www.jianshu.com", "tony"),spider);
+
+        System.out.println("end....");
+    }
+}
