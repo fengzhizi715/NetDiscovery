f694049b3c68cfd7719a25ddc3c347f0adf3fa86
fengzhizi715 <fengzhizi715@126.com>
2019-04-25 18:06:00 +0800

    update HashSetDuplicateFilter 修复并发场景会出现线程安全的问题

--- core/src/main/java/com/cv4j/netdiscovery/core/queue/filter/HashSetDuplicateFilter.java
+++ core/src/main/java/com/cv4j/netdiscovery/core/queue/filter/HashSetDuplicateFilter.java
@@ -2,11 +2,11 @@ package com.cv4j.netdiscovery.core.queue.filter;
 
 import com.cv4j.netdiscovery.core.domain.Request;
 
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.CopyOnWriteArrayList;
 
 /**
  * Created by tony on 2018/1/3.
@@ -14,7 +14,7 @@ import java.util.concurrent.ConcurrentHashMap;
 public class HashSetDuplicateFilter implements DuplicateFilter {
 
     private Set<String> urls = Collections.newSetFromMap(new ConcurrentHashMap<String, Boolean>());
-    private List<String> noNeedCheckDuplicateUrls = new ArrayList<>();
+    private List<String> noNeedCheckDuplicateUrls = new CopyOnWriteArrayList<>();
 
     @Override
     public boolean isDuplicate(Request request) {
