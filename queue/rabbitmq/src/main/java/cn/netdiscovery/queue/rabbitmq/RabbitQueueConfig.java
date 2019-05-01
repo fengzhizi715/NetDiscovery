c86fa040b75bfbff28f1d900108d38d2c02cc7e6
fengzhizi715 <fengzhizi715@126.com>
2019-05-02 00:20:26 +0800

    add rabbitmq module

---
+++ queue/rabbitmq/src/main/java/cn/netdiscovery/queue/rabbitmq/RabbitQueueConfig.java
@@ -0,0 +1,59 @@
+package cn.netdiscovery.queue.rabbitmq;
+
+import lombok.Getter;
+
+import java.util.List;
+import java.util.Properties;
+
+/**
+ * @author bdq
+ * @date 2018-12-28
+ */
+@Getter
+public class RabbitQueueConfig {
+    private Properties producerProperties;
+    private Properties consumeProperties;
+    private String producerExchange;
+    private String consumerExchange;
+    private List<String> queueNames;
+
+    public RabbitQueueConfig(RabbitQueueConfigBuilder builder) {
+        producerProperties = builder.producerProperties;
+        consumeProperties = builder.consumeProperties;
+        producerExchange = builder.producerExchange;
+        consumerExchange=builder.consumerExchange;
+        queueNames = builder.queueNames;
+    }
+
+    public static class RabbitQueueConfigBuilder {
+        private Properties producerProperties;
+        private Properties consumeProperties;
+        private String producerExchange;
+        private String consumerExchange;
+        private List<String> queueNames;
+
+        public RabbitQueueConfigBuilder(Properties producerProperties, Properties consumeProperties) {
+            this.producerProperties = producerProperties;
+            this.consumeProperties = consumeProperties;
+        }
+
+        public RabbitQueueConfigBuilder producerExchange(String producerExchange) {
+            this.producerExchange = producerExchange;
+            return this;
+        }
+
+        public RabbitQueueConfigBuilder consumerExchange(String consumerExchange) {
+            this.consumerExchange = consumerExchange;
+            return this;
+        }
+
+        public RabbitQueueConfigBuilder queueNames(List<String> queueNames) {
+            this.queueNames = queueNames;
+            return this;
+        }
+
+        public RabbitQueueConfig build() {
+            return new RabbitQueueConfig(this);
+        }
+    }
+}
