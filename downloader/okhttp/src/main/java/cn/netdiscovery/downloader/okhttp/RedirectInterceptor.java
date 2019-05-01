f32f4def4506d37806620e5788e93a8fb309606b
fengzhizi715 <fengzhizi715@126.com>
2019-05-01 23:24:49 +0800

    add okhttp module

---
+++ downloader/okhttp/src/main/java/cn/netdiscovery/downloader/okhttp/RedirectInterceptor.java
@@ -0,0 +1,31 @@
+package cn.netdiscovery.downloader.okhttp;
+
+import okhttp3.HttpUrl;
+import okhttp3.Interceptor;
+import okhttp3.Request;
+import okhttp3.Response;
+
+import java.io.IOException;
+
+/**
+ * Created by tony on 2018/2/26.
+ */
+public class RedirectInterceptor implements Interceptor {
+
+    @Override
+    public Response intercept(Chain chain) throws IOException {
+
+        Request request = chain.request();
+        HttpUrl beforeUrl = request.url();
+        Response response = chain.proceed(request);
+        HttpUrl afterUrl = response.request().url();
+        //根据url判断是否是重定向 处理两种情况 1、跨协议 2、原先不是GET请求。
+        if(!beforeUrl.equals(afterUrl)
+                && (!beforeUrl.scheme().equals(afterUrl.scheme())||!"GET".equals(request.method()))) {
+
+            Request newRequest = request.newBuilder().url(response.request().url()).build();
+            response = chain.proceed(newRequest);
+        }
+        return response;
+    }
+}
