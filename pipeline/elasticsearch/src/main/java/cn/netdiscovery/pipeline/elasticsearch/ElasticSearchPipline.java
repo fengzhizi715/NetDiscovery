020676a44756120d1f78b5b432bdcdddc3d547a0
fengzhizi715 <fengzhizi715@126.com>
2019-05-02 12:19:21 +0800

    add es module

---
+++ pipeline/elasticsearch/src/main/java/cn/netdiscovery/pipeline/elasticsearch/ElasticSearchPipline.java
@@ -0,0 +1,45 @@
+package cn.netdiscovery.pipeline.elasticsearch;
+
+import cn.netdiscovery.core.domain.ResultItems;
+import cn.netdiscovery.core.pipeline.Pipeline;
+import lombok.extern.slf4j.Slf4j;
+import org.elasticsearch.action.index.IndexResponse;
+import org.elasticsearch.client.transport.TransportClient;
+import org.elasticsearch.rest.RestStatus;
+
+/**
+ * @author bdq
+ * @date 2018-12-26
+ */
+@Slf4j
+public class ElasticSearchPipline extends Pipeline {
+    private TransportClient client;
+    private String index;
+    private String type;
+
+    public ElasticSearchPipline(TransportClient client, String index, String type) {
+
+        this(client,index,type,0);
+    }
+
+    public ElasticSearchPipline(TransportClient client, String index, String type, int pipelineDelay) {
+
+        super(pipelineDelay);
+        this.client = client;
+        this.index = index;
+        this.type = type;
+    }
+
+    @Override
+    public void process(ResultItems resultItems) {
+        IndexResponse response = client.prepareIndex(index, type)
+                .setSource(resultItems.getAll())
+                .get();
+        RestStatus restStatus = response.status();
+        if (RestStatus.CREATED == restStatus) {
+            log.info("key {} have already created!", response.getId());
+        } else {
+            log.error("an error occured with result id {}!", restStatus.getStatus());
+        }
+    }
+}
