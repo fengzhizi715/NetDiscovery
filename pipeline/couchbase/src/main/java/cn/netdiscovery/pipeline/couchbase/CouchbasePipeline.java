6836348ac70fcfc8c2635ea7f5cba11be9c7093e
fengzhizi715 <fengzhizi715@126.com>
2019-05-02 12:09:20 +0800

    add couchbase module

---
+++ pipeline/couchbase/src/main/java/cn/netdiscovery/pipeline/couchbase/CouchbasePipeline.java
@@ -0,0 +1,61 @@
+package cn.netdiscovery.pipeline.couchbase;
+
+import cn.netdiscovery.core.domain.ResultItems;
+import cn.netdiscovery.core.pipeline.Pipeline;
+import com.couchbase.client.java.Bucket;
+import com.couchbase.client.java.CouchbaseCluster;
+import com.couchbase.client.java.document.JsonDocument;
+import com.couchbase.client.java.document.json.JsonObject;
+
+import java.util.Map;
+
+/**
+ * Created by tony on 2018/2/17.
+ */
+public class CouchbasePipeline extends Pipeline{
+
+    private CouchbaseCluster cluster;
+    private Bucket bucket;
+    private String documentId;
+
+    public CouchbasePipeline(CouchbaseCluster cluster, Bucket bucket, String documentId){
+
+        this(cluster,bucket,documentId,0);
+    }
+
+    public CouchbasePipeline(CouchbaseCluster cluster, Bucket bucket, String documentId, int pipelineDelay){
+
+        super(pipelineDelay);
+        this.cluster = cluster;
+        this.bucket = bucket;
+        this.documentId = documentId;
+    }
+
+    public CouchbasePipeline(CouchbaseCluster cluster, String bucketName, String documentId){
+
+        this(cluster,bucketName,documentId,0);
+    }
+
+    public CouchbasePipeline(CouchbaseCluster cluster, String bucketName, String documentId, int pipelineDelay){
+
+        super(pipelineDelay);
+        this.cluster = cluster;
+        this.bucket = cluster.openBucket(bucketName);
+        this.documentId = documentId;
+    }
+
+    @Override
+    public void process(ResultItems resultItems) {
+
+        JsonObject content = JsonObject.create();
+        for (Map.Entry<String, Object> entry : resultItems.getAll().entrySet()) {
+
+            content.put(entry.getKey(),entry.getValue());
+        }
+
+        bucket.upsert(JsonDocument.create(documentId, content));
+
+        // Close all buckets and disconnect
+        cluster.disconnect();
+    }
+}
