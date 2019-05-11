f74541b365386854765c9677e5c7e4fbd8a01a7f
fengzhizi715 <fengzhizi715@126.com>
2019-05-11 23:28:13 +0800

    add QuartzManager

---
+++ core/src/main/java/cn/netdiscovery/core/quartz/QuartzManager.java
@@ -0,0 +1,138 @@
+package cn.netdiscovery.core.quartz;
+
+import org.quartz.*;
+import org.quartz.impl.StdSchedulerFactory;
+
+/**
+ * Created by tony on 2019-05-11.
+ */
+public class QuartzManager {
+
+    private static SchedulerFactory schedulerFactory = new StdSchedulerFactory();
+
+    /**
+     * 添加一个定时任务
+     *
+     * @param jobName 任务名
+     * @param jobGroupName  任务组名
+     * @param triggerName 触发器名
+     * @param triggerGroupName 触发器组名
+     * @param jobClass  任务
+     * @param cron   时间设置，参考quartz说明文档
+     */
+    public static void addJob(String jobName, String jobGroupName,
+                              String triggerName, String triggerGroupName, Class jobClass, String cron) {
+        try {
+            Scheduler sched = schedulerFactory.getScheduler();
+            // 任务名，任务组，任务执行类
+            JobDetail jobDetail= JobBuilder.newJob(jobClass).withIdentity(jobName, jobGroupName).build();
+
+            // 触发器
+            TriggerBuilder<Trigger> triggerBuilder = TriggerBuilder.newTrigger();
+            // 触发器名,触发器组
+            triggerBuilder.withIdentity(triggerName, triggerGroupName);
+            triggerBuilder.startNow();
+            // 触发器时间设定
+            triggerBuilder.withSchedule(CronScheduleBuilder.cronSchedule(cron));
+            // 创建Trigger对象
+            CronTrigger trigger = (CronTrigger) triggerBuilder.build();
+
+            // 调度容器设置JobDetail和Trigger
+            sched.scheduleJob(jobDetail, trigger);
+
+            // 启动
+            if (!sched.isShutdown()) {
+                sched.start();
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    /**
+     * 修改一个任务的触发时间
+     *
+     * @param jobName
+     * @param jobGroupName
+     * @param triggerName 触发器名
+     * @param triggerGroupName 触发器组名
+     * @param cron   时间设置，参考quartz说明文档
+     */
+    public static void modifyJobTime(String jobName,
+                                     String jobGroupName, String triggerName, String triggerGroupName, String cron) {
+        try {
+            Scheduler sched = schedulerFactory.getScheduler();
+            TriggerKey triggerKey = TriggerKey.triggerKey(triggerName, triggerGroupName);
+            CronTrigger trigger = (CronTrigger) sched.getTrigger(triggerKey);
+            if (trigger == null) {
+                return;
+            }
+
+            String oldTime = trigger.getCronExpression();
+            if (!oldTime.equalsIgnoreCase(cron)) {
+                /** 调用 rescheduleJob 开始 */
+                // 触发器
+                TriggerBuilder<Trigger> triggerBuilder = TriggerBuilder.newTrigger();
+                // 触发器名,触发器组
+                triggerBuilder.withIdentity(triggerName, triggerGroupName);
+                triggerBuilder.startNow();
+                // 触发器时间设定
+                triggerBuilder.withSchedule(CronScheduleBuilder.cronSchedule(cron));
+                // 创建Trigger对象
+                trigger = (CronTrigger) triggerBuilder.build();
+                sched.rescheduleJob(triggerKey, trigger);
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    /**
+     * 移除一个任务
+     *
+     * @param jobName
+     * @param jobGroupName
+     * @param triggerName
+     * @param triggerGroupName
+     */
+    public static void removeJob(String jobName, String jobGroupName,
+                                 String triggerName, String triggerGroupName) {
+        try {
+            Scheduler sched = schedulerFactory.getScheduler();
+
+            TriggerKey triggerKey = TriggerKey.triggerKey(triggerName, triggerGroupName);
+
+            sched.pauseTrigger(triggerKey);// 停止触发器
+            sched.unscheduleJob(triggerKey);// 移除触发器
+            sched.deleteJob(JobKey.jobKey(jobName, jobGroupName));// 删除任务
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    /**
+     * 启动所有定时任务
+     */
+    public static void startJobs() {
+        try {
+            Scheduler sched = schedulerFactory.getScheduler();
+            sched.start();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    /**
+     * 关闭所有定时任务
+     */
+    public static void shutdownJobs() {
+        try {
+            Scheduler sched = schedulerFactory.getScheduler();
+            if (!sched.isShutdown()) {
+                sched.shutdown();
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
