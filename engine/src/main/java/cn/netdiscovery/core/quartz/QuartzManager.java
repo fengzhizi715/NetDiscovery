0fd114c55c7b8de01a0b944ce9d7d8760c937d56
fengzhizi715 <fengzhizi715@126.com>
2019-05-18 12:52:26 +0800

    update engine module

--- engine/src/main/java/cn/netdiscovery/core/quartz/QuartzManager.java
+++ engine/src/main/java/cn/netdiscovery/core/quartz/QuartzManager.java
@@ -2,19 +2,9 @@ package cn.netdiscovery.core.quartz;
 
 import cn.netdiscovery.core.Spider;
 import cn.netdiscovery.core.domain.Request;
-
 import cn.netdiscovery.core.domain.bean.SpiderJobBean;
 import cn.netdiscovery.core.exception.SpiderException;
-import org.quartz.CronScheduleBuilder;
-import org.quartz.CronTrigger;
-import org.quartz.JobBuilder;
-import org.quartz.JobDetail;
-import org.quartz.JobKey;
-import org.quartz.Scheduler;
-import org.quartz.SchedulerFactory;
-import org.quartz.Trigger;
-import org.quartz.TriggerBuilder;
-import org.quartz.TriggerKey;
+import org.quartz.*;
 import org.quartz.impl.StdSchedulerFactory;
 
 import java.util.Map;
