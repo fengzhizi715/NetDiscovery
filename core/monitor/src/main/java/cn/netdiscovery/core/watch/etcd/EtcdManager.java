14c4b6f6f97302f479cfdcd5412c2b3d2b7125b7
fengzhizi715 <fengzhizi715@126.com>
2019-06-12 23:23:57 +0800

    update EtcdManager

--- core/monitor/src/main/java/cn/netdiscovery/core/watch/etcd/EtcdManager.java
+++ core/monitor/src/main/java/cn/netdiscovery/core/watch/etcd/EtcdManager.java
@@ -2,8 +2,16 @@ package cn.netdiscovery.core.watch.etcd;
 
 import cn.netdiscovery.core.config.Configuration;
 import com.safframework.tony.common.utils.Preconditions;
+import io.etcd.jetcd.ByteSequence;
 import io.etcd.jetcd.Client;
+import io.etcd.jetcd.Watch;
+import io.etcd.jetcd.options.WatchOption;
+import io.etcd.jetcd.watch.WatchEvent;
 import lombok.extern.slf4j.Slf4j;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Optional;
+import java.util.concurrent.CountDownLatch;
 
 
 /**
@@ -28,35 +36,35 @@ public class EtcdManager {
 
     public void process() {
 
-//        CountDownLatch latch = new CountDownLatch(Integer.MAX_VALUE);
-//        Watch.Watcher watcher = null;
-//
-//        try {
-//            ByteSequence watchKey = ByteSequence.from("/netdiscovery/", StandardCharsets.UTF_8);
-//            WatchOption watchOpts = WatchOption.newBuilder().withRevision(0).withPrefix(ByteSequence.from("/netdiscovery/".getBytes())).build();
-//
-//            watcher = client.getWatchClient().watch(watchKey, watchOpts, response -> {
-//                        for (WatchEvent event : response.getEvents()) {
-//                            log.info("type={}, key={}, value={}",
-//                                    event.getEventType().toString(),
-//                                    Optional.ofNullable(event.getKeyValue().getKey())
-//                                            .map(bs -> bs.toString(StandardCharsets.UTF_8))
-//                                            .orElse(""),
-//                                    Optional.ofNullable(event.getKeyValue().getValue())
-//                                            .map(bs -> bs.toString(StandardCharsets.UTF_8))
-//                                            .orElse(""));
-//                        }
-//
-//                        latch.countDown();
-//                    }
-//            );
-//
-//            latch.await();
-//        } catch (Exception e) {
-//            if (watcher != null) {
-//                watcher.close();
-//            }
-//        }
+        CountDownLatch latch = new CountDownLatch(Integer.MAX_VALUE);
+        Watch.Watcher watcher = null;
+
+        try {
+            ByteSequence watchKey = ByteSequence.from("//netdiscovery", StandardCharsets.UTF_8);
+            WatchOption watchOpts = WatchOption.newBuilder().withRevision(0).withPrefix(ByteSequence.from("//netdiscovery".getBytes())).build();
+
+            watcher = client.getWatchClient().watch(watchKey, watchOpts, response -> {
+                        for (WatchEvent event : response.getEvents()) {
+                            log.info("type={}, key={}, value={}",
+                                    event.getEventType().toString(),
+                                    Optional.ofNullable(event.getKeyValue().getKey())
+                                            .map(bs -> bs.toString(StandardCharsets.UTF_8))
+                                            .orElse(""),
+                                    Optional.ofNullable(event.getKeyValue().getValue())
+                                            .map(bs -> bs.toString(StandardCharsets.UTF_8))
+                                            .orElse(""));
+                        }
+
+                        latch.countDown();
+                    }
+            );
+
+            latch.await();
+        } catch (Exception e) {
+            if (watcher != null) {
+                watcher.close();
+            }
+        }
     }
 
 }
