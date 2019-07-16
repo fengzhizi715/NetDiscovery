7e6f1cc81a468c917ff578db531a0305650fcf6f
fengzhizi715 <fengzhizi715@126.com>
2019-07-17 01:36:40 +0800

    update Producer

--- queue/rocketmq/src/main/java/cn/netdiscovery/queue/rocketmq/Producer.java
+++ queue/rocketmq/src/main/java/cn/netdiscovery/queue/rocketmq/Producer.java
@@ -1,7 +1,14 @@
 package cn.netdiscovery.queue.rocketmq;
 
+import cn.netdiscovery.core.domain.Request;
+import cn.netdiscovery.core.utils.SerializableUtils;
+import lombok.Getter;
+import org.apache.rocketmq.client.exception.MQBrokerException;
 import org.apache.rocketmq.client.exception.MQClientException;
 import org.apache.rocketmq.client.producer.DefaultMQProducer;
+import org.apache.rocketmq.client.producer.SendResult;
+import org.apache.rocketmq.common.message.Message;
+import org.apache.rocketmq.remoting.exception.RemotingException;
 
 /**
  * Created by tony on 2019-07-16.
@@ -10,10 +17,15 @@ public class Producer {
 
     private DefaultMQProducer producer;
 
-    public Producer(String producerName,String nameServerAddress,int retryTimes) {
+    @Getter
+    private String tags;
+
+    public Producer(String producerName,String nameServerAddress,int retryTimes,String tags) {
+
         this.producer = new DefaultMQProducer(producerName);
         this.producer.setNamesrvAddr(nameServerAddress);
         this.producer.setRetryTimesWhenSendAsyncFailed(retryTimes);
+        this.tags = tags;
     }
 
     public void start() {
@@ -25,4 +37,23 @@ public class Producer {
         }
     }
 
+    public SendResult send(Request request) {
+
+        Message msg = new Message(request.getSpiderName(), tags, SerializableUtils.toJson(request).getBytes());
+
+        try {
+            SendResult sendResult = producer.send(msg);
+            return sendResult;
+        } catch (MQClientException e) {
+            e.printStackTrace();
+        } catch (RemotingException e) {
+            e.printStackTrace();
+        } catch (MQBrokerException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
 }
