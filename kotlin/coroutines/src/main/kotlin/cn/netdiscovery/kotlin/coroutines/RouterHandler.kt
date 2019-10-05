1d5b62d9900ae1690bd579c7f54b1b7d414eedcc
fengzhizi715 <fengzhizi715@126.com>
2019-10-06 01:29:04 +0800

    add Rpc.kt

---
+++ kotlin/coroutines/src/main/kotlin/cn/netdiscovery/kotlin/coroutines/RouterHandler.kt
@@ -0,0 +1,243 @@
+package cn.netdiscovery.kotlin.coroutines
+
+import cn.netdiscovery.core.Spider
+import cn.netdiscovery.core.config.Constant.*
+import cn.netdiscovery.core.constants.ResponseCode
+import cn.netdiscovery.core.domain.bean.SpiderBean
+import cn.netdiscovery.core.domain.bean.SpiderJobBean
+import cn.netdiscovery.core.domain.response.JobsResponse
+import cn.netdiscovery.core.domain.response.SpiderResponse
+import cn.netdiscovery.core.domain.response.SpiderStatusResponse
+import cn.netdiscovery.core.domain.response.SpidersResponse
+import cn.netdiscovery.core.utils.SerializableUtils
+import cn.netdiscovery.core.vertx.VertxUtils
+import com.safframework.tony.common.utils.Preconditions
+import io.vertx.ext.web.Router
+import io.vertx.ext.web.client.WebClient
+import io.vertx.ext.web.handler.StaticHandler
+import io.vertx.micrometer.PrometheusScrapingHandler
+import java.net.InetAddress
+import java.net.UnknownHostException
+import java.util.ArrayList
+
+/**
+ * Created by tony on 2019-08-12.
+ */
+class RouterHandler(private val spiders: Map<String, Spider>, private val jobs: Map<String, SpiderJobBean>, private val router: Router, private val useMonitor: Boolean) {
+
+    fun route() {
+
+        // 检测 SpiderEngine 的健康状况
+        router.route("/netdiscovery/health/").handler { routingContext ->
+
+            val response = routingContext.response()
+            response.putHeader(CONTENT_TYPE, CONTENT_TYPE_JSON)
+
+            response.end(SerializableUtils.toJson(cn.netdiscovery.core.domain.response.HttpResponse.Ok))
+        }
+
+        router.route("/netdiscovery/metrics").handler(PrometheusScrapingHandler.create())
+
+        if (Preconditions.isNotBlank(spiders)) {
+
+            // 显示容器下所有爬虫的信息
+            router.route("/netdiscovery/spiders/").handler { routingContext ->
+
+                val response = routingContext.response()
+                response.putHeader(CONTENT_TYPE, CONTENT_TYPE_JSON)
+
+                val list = ArrayList<SpiderBean>()
+
+                var spider: Spider? = null
+                var entity: SpiderBean? = null
+
+                for ((_, value) in spiders) {
+
+                    spider = value
+
+                    entity = SpiderBean()
+                    entity.spiderName = spider.name
+                    entity.spiderStatus = spider.spiderStatus
+                    entity.leftRequestSize = spider.queue.getLeftRequests(spider.name)
+                    entity.totalRequestSize = spider.queue.getTotalRequests(spider.name)
+                    entity.consumedRequestSize = entity.totalRequestSize - entity.leftRequestSize
+                    entity.queueType = spider.queue.javaClass.simpleName
+                    entity.downloaderType = spider.downloader.javaClass.simpleName
+                    list.add(entity)
+                }
+
+                val spidersResponse = SpidersResponse()
+                spidersResponse.code = OK_STATUS_CODE
+                spidersResponse.message = SUCCESS
+                spidersResponse.data = list
+
+                // 写入响应并结束处理
+                response.end(SerializableUtils.toJson(spidersResponse))
+            }
+
+            // 根据爬虫的名称获取爬虫的详情
+            router.route("/netdiscovery/spider/:spiderName/detail").handler { routingContext ->
+
+                val response = routingContext.response()
+                response.putHeader(CONTENT_TYPE, CONTENT_TYPE_JSON)
+
+                val spiderName = routingContext.pathParam("spiderName")
+
+                if (Preconditions.isNotBlank(spiderName) && spiders[spiderName] != null) {
+
+                    val spider = spiders[spiderName]
+
+                    val entity = SpiderBean()
+                    entity.spiderName = spiderName
+                    entity.spiderStatus = spider.getSpiderStatus()
+                    entity.leftRequestSize = spider.getQueue().getLeftRequests(spiderName)
+                    entity.totalRequestSize = spider.getQueue().getTotalRequests(spiderName)
+                    entity.consumedRequestSize = entity.totalRequestSize - entity.leftRequestSize
+                    entity.queueType = spider.getQueue().javaClass.simpleName
+                    entity.downloaderType = spider.getDownloader().javaClass.simpleName
+
+                    val spiderResponse = SpiderResponse()
+                    spiderResponse.code = OK_STATUS_CODE
+                    spiderResponse.message = SUCCESS
+                    spiderResponse.data = entity
+
+                    // 写入响应并结束处理
+                    response.end(SerializableUtils.toJson(spiderResponse))
+                } else {
+
+                    response.end(SerializableUtils.toJson(cn.netdiscovery.core.domain.response.HttpResponse(ResponseCode.SpiderNotFound)))
+                }
+            }
+
+            // 修改单个爬虫的状态
+            router.post("/netdiscovery/spider/:spiderName/status").handler { routingContext ->
+
+                val response = routingContext.response()
+                response.putHeader(CONTENT_TYPE, CONTENT_TYPE_JSON)
+
+                val spiderName = routingContext.pathParam("spiderName")
+
+                if (Preconditions.isNotBlank(spiderName) && spiders[spiderName] != null) {
+
+                    val json = routingContext.bodyAsJson
+                    var spiderStatusResponse: SpiderStatusResponse? = null
+
+                    val spider = spiders[spiderName]
+
+                    if (json != null) {
+
+                        val status = json.getInteger("status")!!
+
+                        spiderStatusResponse = SpiderStatusResponse()
+
+                        when (status) {
+
+                            Spider.SPIDER_STATUS_PAUSE -> {
+                                spider.pause()
+                                spiderStatusResponse.data = String.format("SpiderEngine pause Spider %s success", spider.getName())
+                            }
+
+                            Spider.SPIDER_STATUS_RESUME -> {
+                                spider.resume()
+                                spiderStatusResponse.data = String.format("SpiderEngine resume Spider %s success", spider.getName())
+                            }
+
+                            Spider.SPIDER_STATUS_STOPPED -> {
+                                spider.forceStop()
+                                spiderStatusResponse.data = String.format("SpiderEngine stop Spider %s success", spider.getName())
+                            }
+
+                            else -> {
+                            }
+                        }
+                    }
+
+                    spiderStatusResponse!!.code = OK_STATUS_CODE
+                    spiderStatusResponse.message = SUCCESS
+
+                    // 写入响应并结束处理
+                    response.end(SerializableUtils.toJson(spiderStatusResponse))
+                } else {
+
+                    response.end(SerializableUtils.toJson(cn.netdiscovery.core.domain.response.HttpResponse(ResponseCode.SpiderNotFound)))
+                }
+
+            }
+
+            // 添加新的url任务到某个正在运行中的爬虫
+            router.post("/netdiscovery/spider/:spiderName/push").handler { routingContext ->
+
+                val response = routingContext.response()
+                response.putHeader(CONTENT_TYPE, CONTENT_TYPE_JSON)
+
+                val spiderName = routingContext.pathParam("spiderName")
+
+                if (Preconditions.isNotBlank(spiderName) && spiders[spiderName] != null) {
+
+                    val json = routingContext.bodyAsJson
+
+                    val url = json.getString("url")
+
+                    val spider = spiders[spiderName]
+                    spider.getQueue().pushToRunninSpider(url, spider)
+
+                    response.end(SerializableUtils.toJson(cn.netdiscovery.core.domain.response.HttpResponse("待抓取的url已经放入queue中")))
+                } else {
+
+                    response.end(SerializableUtils.toJson(cn.netdiscovery.core.domain.response.HttpResponse(ResponseCode.SpiderNotFound)))
+                }
+
+            }
+
+            // 显示所有爬虫的定时任务
+            router.route("/netdiscovery/jobs/").handler { routingContext ->
+
+                val response = routingContext.response()
+                response.putHeader(CONTENT_TYPE, CONTENT_TYPE_JSON)
+
+                val list = ArrayList<SpiderJobBean>()
+
+                list.addAll(jobs.values)
+
+                val jobsResponse = JobsResponse()
+                jobsResponse.code = OK_STATUS_CODE
+                jobsResponse.message = SUCCESS
+                jobsResponse.data = list
+
+                // 写入响应并结束处理
+                response.end(SerializableUtils.toJson(jobsResponse))
+            }
+
+            if (useMonitor) { // 是否使用 agent
+
+                // The web server handler
+                router.route().handler(StaticHandler.create().setCachingEnabled(false))
+
+                // The proxy handler
+                val client = WebClient.create(VertxUtils.getVertx())
+
+                var localhost: InetAddress? = null
+                try {
+                    localhost = InetAddress.getLocalHost()
+                } catch (e: UnknownHostException) {
+                    e.printStackTrace()
+                }
+
+                val get = client.get(8081, localhost!!.hostAddress, "/netdiscovery/dashboard/")
+                router.get("/dashboard").handler { ctx ->
+                    get.send{ ar ->
+                        if (ar.succeeded()) {
+                            val result = ar.result()
+                            (ctx.response().statusCode = result.statusCode())
+                                    .putHeader(CONTENT_TYPE, CONTENT_TYPE_JSON)
+                                    .end(result.body())
+                        } else {
+                            ctx.fail(ar.cause())
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+}
