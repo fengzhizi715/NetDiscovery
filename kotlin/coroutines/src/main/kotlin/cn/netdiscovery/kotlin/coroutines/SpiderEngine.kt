eaca7b19a56dddf8ad1f0ac4bf54b65694ee403e
fengzhizi715 <fengzhizi715@126.com>
2019-10-06 15:08:10 +0800

    update SpiderEngine.kt

--- kotlin/coroutines/src/main/kotlin/cn/netdiscovery/kotlin/coroutines/SpiderEngine.kt
+++ kotlin/coroutines/src/main/kotlin/cn/netdiscovery/kotlin/coroutines/SpiderEngine.kt
@@ -1,27 +1,43 @@
 package cn.netdiscovery.kotlin.coroutines
 
-import com.alibaba.fastjson.JSON
+import cn.netdiscovery.core.RouterHandler
+import cn.netdiscovery.core.Spider
+import cn.netdiscovery.core.config.Configuration
 import cn.netdiscovery.core.config.Constant
-import cn.netdiscovery.core.domain.bean.SpiderBean
-import cn.netdiscovery.core.domain.response.SpiderResponse
-import cn.netdiscovery.core.domain.response.SpiderStatusResponse
-import cn.netdiscovery.core.domain.response.SpidersResponse
+import cn.netdiscovery.core.config.Constant.*
+import cn.netdiscovery.core.domain.Request
+import cn.netdiscovery.core.domain.bean.SpiderJobBean
+import cn.netdiscovery.core.quartz.ProxyPoolJob
+import cn.netdiscovery.core.quartz.QuartzManager
+import cn.netdiscovery.core.quartz.SpiderJob
 import cn.netdiscovery.core.queue.Queue
+import cn.netdiscovery.core.registry.Registry
+import cn.netdiscovery.core.utils.BooleanUtils
+import cn.netdiscovery.core.utils.NumberUtils
 import cn.netdiscovery.core.utils.UserAgent
 import cn.netdiscovery.core.vertx.VertxUtils
+import com.cv4j.proxy.ProxyManager
 import com.cv4j.proxy.ProxyPool
 import com.cv4j.proxy.domain.Proxy
-import com.safframework.tony.common.collection.NoEmptyHashMap
 import com.safframework.tony.common.utils.IOUtils
 import com.safframework.tony.common.utils.Preconditions
 import io.reactivex.Flowable
 import io.reactivex.schedulers.Schedulers
+import io.vertx.core.Handler
+import io.vertx.core.VertxOptions
 import io.vertx.core.http.HttpServer
+import io.vertx.core.http.HttpServerRequest
 import io.vertx.ext.web.Router
 import io.vertx.ext.web.handler.BodyHandler
-import kotlinx.coroutines.runBlocking
+import io.vertx.micrometer.MicrometerMetricsOptions
+import io.vertx.micrometer.VertxPrometheusOptions
 import lombok.Getter
+import java.io.FileNotFoundException
+import java.io.IOException
 import java.util.*
+import java.util.concurrent.ConcurrentHashMap
+import java.util.concurrent.atomic.AtomicInteger
+import java.util.stream.Stream
 
 /**
  * Created by tony on 2018/8/8.
@@ -29,9 +45,21 @@ import java.util.*
 class SpiderEngine private constructor(@field:Getter
                                        val queue: Queue? = null) {
 
-    private val spiders = NoEmptyHashMap<String, Spider>()
+    private var server: HttpServer? = null
 
-    private lateinit var server: HttpServer
+    private var useMonitor = false
+
+    private var registerConsumer: RegisterConsumer? = null
+
+    private var registry: Registry? = null
+
+    private var defaultHttpdPort = 8715 // SpiderEngine 默认的端口号
+
+    private val count = AtomicInteger(0)
+
+    private val spiders = ConcurrentHashMap<String, Spider>()
+
+    private val jobs = ConcurrentHashMap<String, SpiderJobBean>()
 
     init {
 
@@ -49,28 +77,58 @@ class SpiderEngine private constructor(@field:Getter
 
             Arrays.asList(*uaList)
                     .parallelStream()
-                    .forEach {
+                    .forEach { name ->
 
-                        str -> this.javaClass.getResourceAsStream(str)?.use {
+                        try {
+                            this.javaClass.getResourceAsStream(name)?.let {
+                                val inputString = IOUtils.inputStream2String(it) // it 无须关闭，inputStream2String()方法里已经做了关闭流的操作
+                                if (Preconditions.isNotBlank(inputString)) {
+                                    val ss = inputString.split("\r\n".toRegex()).dropLastWhile { it.isEmpty() }.toTypedArray()
+                                    if (ss.size > 0) {
 
-                          IOUtils.inputStream2String(it)?.let {
-
-                                val ss = it.split("\r\n".toRegex()).dropLastWhile { str.isEmpty() }.toTypedArray()
-                                if (ss.isNotEmpty()) {
-
-                                    Arrays.asList(*ss).forEach {
-                                        UserAgent.uas.add(it)
+                                        Arrays.asList(*ss).forEach { ua -> UserAgent.uas.add(ua) }
                                     }
                                 }
                             }
+                        } catch (e: FileNotFoundException) {
+                            e.printStackTrace()
+                        } catch (e: IOException) {
+                            e.printStackTrace()
                         }
                     }
+
+            com.cv4j.proxy.config.Constant.setUas(UserAgent.uas) // 让代理池也能够共享ua
         }
+
+        try {
+            defaultHttpdPort = NumberUtils.toInt(Configuration.getConfig("spiderEngine.config.port"))
+            useMonitor = BooleanUtils.toBoolean(Configuration.getConfig("spiderEngine.config.useMonitor"))
+        } catch (e: ClassCastException) {
+            defaultHttpdPort = 8715
+            useMonitor = false
+        }
+
+        VertxUtils.configVertx(VertxOptions().setMetricsOptions(
+                MicrometerMetricsOptions()
+                        .setPrometheusOptions(VertxPrometheusOptions().setEnabled(true))
+                        .setEnabled(true)))
     }
 
     fun proxyList(proxies: List<Proxy>): SpiderEngine {
 
         ProxyPool.addProxyList(proxies)
+        return this
+    }
+
+    fun setUseMonitor(useMonitor: Boolean): SpiderEngine {
+
+        this.useMonitor = useMonitor
+        return this
+    }
+
+    fun setRegistry(registry: Registry): SpiderEngine {
+
+        this.registry = registry
         return this
     }
 
@@ -82,11 +140,9 @@ class SpiderEngine private constructor(@field:Getter
      */
     fun addSpider(spider: Spider?): SpiderEngine {
 
-        if (spider != null) {
+        if (spider != null && !spiders.containsKey(spider.name)) {
 
-            if (!spiders.containsKey(spider.name)) {
-                spiders[spider.name] = spider
-            }
+            spiders[spider.name] = spider
         }
         return this
     }
@@ -111,158 +167,48 @@ class SpiderEngine private constructor(@field:Getter
 
     /**
      * 对各个爬虫的状态进行监测，并返回json格式。
-     * 如果要使用此方法，须放在run()/runWithRepeat()之前
+     * 如果要使用此方法，必须要放在run()之前
      *
      * @param port
      */
-    fun httpd(port: Int): SpiderEngine {
+    @JvmOverloads
+    fun httpd(port: Int = defaultHttpdPort): SpiderEngine {
 
+        defaultHttpdPort = port
         server = VertxUtils.getVertx().createHttpServer()
 
         val router = Router.router(VertxUtils.getVertx())
         router.route().handler(BodyHandler.create())
 
-        if (Preconditions.isNotBlank<Map<String, Spider>>(spiders)) {
+        val routerHandler = RouterHandler(spiders, jobs, router, useMonitor)
+        routerHandler.route()
 
-            for ((_, spider) in spiders) {
+        server!!.requestHandler(Handler<HttpServerRequest> { router.accept(it) }).listen(port)
 
-                router.route("/netdiscovery/spider/" + spider.name).handler{ routingContext ->
+        return this
+    }
 
-                    // 所有的请求都会调用这个处理器处理
-                    val response = routingContext.response()
-                    response.putHeader(Constant.CONTENT_TYPE, Constant.CONTENT_TYPE_JSON)
+    /**
+     * 注册 Vert.x eventBus 的消费者
+     * @param registerConsumer
+     * @return
+     */
+    fun registerConsumers(registerConsumer: RegisterConsumer): SpiderEngine {
 
-                    val entity = SpiderBean()
-                    entity.spiderName = spider.name
-                    entity.spiderStatus = spider.spiderStatus
-                    entity.leftRequestSize = spider.queue.getLeftRequests(spider.name)
-                    entity.totalRequestSize = spider.queue.getTotalRequests(spider.name)
-                    entity.consumedRequestSize = entity.totalRequestSize - entity.leftRequestSize
-                    entity.queueType = spider.queue.javaClass.simpleName
-                    entity.downloaderType = spider.downloader.javaClass.simpleName
-
-                    val spiderResponse = SpiderResponse()
-                    spiderResponse.code = Constant.OK_STATUS_CODE
-                    spiderResponse.message = Constant.SUCCESS
-                    spiderResponse.data = entity
-
-                    // 写入响应并结束处理
-                    response.end(JSON.toJSONString(spiderResponse))
-                }
-
-                router.post("/netdiscovery/spider/" + spider.name + "/status").handler{ routingContext ->
-
-                    // 所有的请求都会调用这个处理器处理
-                    val response = routingContext.response()
-                    response.putHeader(Constant.CONTENT_TYPE, Constant.CONTENT_TYPE_JSON)
-
-                    val json = routingContext.getBodyAsJson()
-
-                    var spiderStatusResponse: SpiderStatusResponse? = null
-
-                    if (json != null) {
-
-                        val status = json.getInteger("status")
-
-                        spiderStatusResponse = SpiderStatusResponse()
-
-                        when (status) {
-
-                            Spider.SPIDER_STATUS_PAUSE -> {
-                                spider.pause()
-                                spiderStatusResponse.data = "SpiderEngine pause Spider ${spider.name} success"
-                            }
-
-                            Spider.SPIDER_STATUS_RESUME -> {
-                                spider.resume()
-                                spiderStatusResponse.data = "SpiderEngine resume Spider ${spider.name} success"
-                            }
-
-                            Spider.SPIDER_STATUS_STOPPED -> {
-                                spider.forceStop()
-                                spiderStatusResponse.data = "SpiderEngine stop Spider ${spider.name} success"
-                            }
-
-                            else -> {
-                            }
-                        }
-                    }
-
-                    spiderStatusResponse!!.code = Constant.OK_STATUS_CODE
-                    spiderStatusResponse.message = Constant.SUCCESS
-
-                    // 写入响应并结束处理
-                    response.end(JSON.toJSONString(spiderStatusResponse))
-                }
-            }
-
-            router.route("/netdiscovery/spiders/").handler{ routingContext ->
-
-                // 所有的请求都会调用这个处理器处理
-                val response = routingContext.response()
-                response.putHeader(Constant.CONTENT_TYPE, Constant.CONTENT_TYPE_JSON)
-
-                val list = ArrayList<SpiderBean>()
-
-                var spider: Spider? = null
-                var entity: SpiderBean? = null
-
-                for ((_, value) in spiders) {
-
-                    spider = value
-
-                    entity = SpiderBean()
-                    entity.spiderName = spider.name
-                    entity.spiderStatus = spider.spiderStatus
-                    entity.leftRequestSize = spider.queue.getLeftRequests(spider.name)
-                    entity.totalRequestSize = spider.queue.getTotalRequests(spider.name)
-                    entity.consumedRequestSize = entity.totalRequestSize - entity.leftRequestSize
-                    entity.queueType = spider.queue.javaClass.simpleName
-                    entity.downloaderType = spider.downloader.javaClass.simpleName
-                    list.add(entity)
-                }
-
-                val spidersResponse = SpidersResponse()
-                spidersResponse.code = Constant.OK_STATUS_CODE
-                spidersResponse.message = Constant.SUCCESS
-                spidersResponse.data = list
-
-                // 写入响应并结束处理
-                response.end(JSON.toJSONString(spidersResponse))
-            }
-        }
-
-        server.requestHandler{ router.accept(it) }.listen(port)
-
+        this.registerConsumer = registerConsumer
         return this
     }
 
     /**
      * 关闭HttpServer
      */
-    fun closeHttpServer() = server?.close()
+    fun closeHttpServer() {
 
-//    /**
-//     * 启动SpiderEngine中所有的spider，让每个爬虫并行运行起来
-//     * 如果在SpiderEngine中某个Spider使用了repeateRequest()，则须使用runWithRepeat()
-//     */
-//    fun run() {
-//
-//        if (Preconditions.isNotBlank<Map<String, Spider>>(spiders)) {
-//
-//            spiders.entries
-//                    .forEach{
-//                        GlobalScope.launch {
-//                            it.value.run()
-//                        }
-//                    }
-//
-//            Runtime.getRuntime().addShutdownHook(Thread {
-//                println("stop all spiders")
-//                stopSpiders()
-//            })
-//        }
-//    }
+        if (server != null) {
+
+            server!!.close()
+        }
+    }
 
     /**
      * 启动SpiderEngine中所有的spider，让每个爬虫并行运行起来。
@@ -270,28 +216,42 @@ class SpiderEngine private constructor(@field:Getter
      */
     fun run() {
 
+        println("\r\n" +
+                "   _   _      _   ____  _\n" +
+                "  | \\ | | ___| |_|  _ \\(_)___  ___ _____   _____ _ __ _   _\n" +
+                "  |  \\| |/ _ \\ __| | | | / __|/ __/ _ \\ \\ / / _ \\ '__| | | |\n" +
+                "  | |\\  |  __/ |_| |_| | \\__ \\ (_| (_) \\ V /  __/ |  | |_| |\n" +
+                "  |_| \\_|\\___|\\__|____/|_|___/\\___\\___/ \\_/ \\___|_|   \\__, |\n" +
+                "                                                      |___/")
+
         if (Preconditions.isNotBlank<Map<String, Spider>>(spiders)) {
 
-            runBlocking {
+            if (registry != null && registry!!.provider != null) {
 
-                Flowable.fromIterable(spiders.toMap().values)
-                        .parallel(spiders.values.size)
-                        .runOn(Schedulers.io())
-                        .map { spider ->
-                            spider.run()
-                            spider
-                        }
-                        .sequential()
-                        .subscribe()
-
-                Runtime.getRuntime().addShutdownHook(Thread {
-                    println("stop all spiders")
-                    stopSpiders()
-                })
+                registry!!.register(registry!!.provider, defaultHttpdPort)
             }
 
-        }
+            registerConsumer?.let {
+                it.process()
+            }
 
+            Flowable.fromIterable(spiders.values)
+                    .parallel(spiders.values.size)
+                    .runOn(Schedulers.io())
+                    .map { spider ->
+                        spider.run()
+
+                        spider
+                    }
+                    .sequential()
+                    .subscribe({ }, { throwable -> throwable.printStackTrace() }, { })
+
+            Runtime.getRuntime().addShutdownHook(Thread {
+                println("stop all spiders")
+                stopSpiders()
+                QuartzManager.shutdownJobs()
+            })
+        }
     }
 
     /**
@@ -299,35 +259,135 @@ class SpiderEngine private constructor(@field:Getter
      *
      * @param name
      */
-    fun getSpider(name: String): Spider? = spiders[name]
+    fun getSpider(name: String): Spider? {
+
+        return spiders[name]
+    }
 
     /**
      * 停止某个爬虫程序
      *
      * @param name
      */
-    fun stopSpider(name: String) = spiders[name]?.stop()
+    fun stopSpider(name: String) {
+
+        val spider = spiders[name]
+
+        spider?.stop()
+    }
 
     /**
      * 停止所有的爬虫程序
      */
     fun stopSpiders() {
 
-        if (Preconditions.isNotBlank<Map<String, Spider>>(spiders)) {
+        if (Preconditions.isNotBlank(spiders)) {
 
-            spiders.forEach { _, spider -> spider.stop() }
+            spiders.forEach { (s, spider) -> spider.stop() }
         }
+    }
+
+    /**
+     * 给 Spider 发起定时任务
+     * @param spiderName
+     * @param cron cron表达式
+     * @param urls
+     */
+    fun addSpiderJob(spiderName: String, cron: String, vararg urls: String): SpiderJobBean? {
+
+        if (Preconditions.isNotBlank(urls)
+                && spiders[spiderName] != null
+                && Preconditions.isNotBlank(cron)) {
+
+            val requests = arrayOfNulls<Request>(urls.size)
+
+            for (i in urls.indices) {
+
+                requests[i] = Request(urls[i], spiderName).checkDuplicate(false)
+            }
+
+            return addSpiderJob(spiderName, cron, requests)
+        }
+
+        return null
+    }
+
+    /**
+     * 给 Spider 发起定时任务
+     * @param spiderName
+     * @param cron cron表达式
+     * @param requests
+     */
+    fun addSpiderJob(spiderName: String, cron: String, requests: Array<Request?>): SpiderJobBean? {
+
+        val spider = spiders[spiderName]
+
+        if (spider != null) {
+            val jobName = SPIDER_JOB_NAME + count.incrementAndGet()
+
+            val jobBean = SpiderJobBean()
+            jobBean.jobName = jobName
+            jobBean.jobGroupName = JOB_GROUP_NAME
+            jobBean.triggerName = TRIGGER_NAME
+            jobBean.triggerGroupName = TRIGGER_GROUP_NAME
+            jobBean.cron = cron
+            jobBean.requests = requests
+
+            Stream.of(*requests)
+                    .filter { it!=null }
+                    .filter { request -> request!!.isCheckDuplicate }
+                    .forEach { request -> request!!.checkDuplicate(false) }
+
+            jobs[jobName] = jobBean
+            QuartzManager.addJob(jobBean, SpiderJob::class.java, cron, spider, *requests)
+
+            return jobBean
+        }
+
+        return null
+    }
+
+    /**
+     * 给 ProxyPool 发起定时任务
+     * @param proxyMap
+     * @param cron cron表达式
+     * @return
+     */
+    fun addProxyPoolJob(proxyMap: Map<String, Class<*>>, cron: String) {
+
+        val jobName = PROXY_POOL_JOB_NAME + count.incrementAndGet()
+
+        QuartzManager.addJob(jobName, JOB_GROUP_NAME, TRIGGER_NAME, TRIGGER_GROUP_NAME, ProxyPoolJob::class.java, cron, proxyMap)
+    }
+
+    /**
+     * 需要在启动 SpiderEngine 之前，启动 ProxyPool
+     */
+    fun startProxyPool(proxyMap: Map<String, Class<*>>?) {
+
+        if (proxyMap == null) return
+
+        ProxyPool.proxyMap = proxyMap
+        val proxyManager = ProxyManager.get()
+        proxyManager.start()
+    }
+
+    /**
+     * 注册 Vert.x eventBus 的消费者
+     */
+    @FunctionalInterface
+    interface RegisterConsumer {
+
+        fun process()
     }
 
     companion object {
 
-        @JvmStatic
         fun create(): SpiderEngine {
 
             return SpiderEngine()
         }
 
-        @JvmStatic
         fun create(queue: Queue): SpiderEngine {
 
             return SpiderEngine(queue)
