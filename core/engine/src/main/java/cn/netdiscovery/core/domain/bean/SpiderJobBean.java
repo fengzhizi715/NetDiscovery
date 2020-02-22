4a046cf4da51012bbe0579e9f6de49a1081f29e3
fengzhizi715 <fengzhizi715@126.com>
2020-02-22 17:50:42 +0800

    update build.gradle

--- core/engine/src/main/java/cn/netdiscovery/core/domain/bean/SpiderJobBean.java
+++ core/engine/src/main/java/cn/netdiscovery/core/domain/bean/SpiderJobBean.java
@@ -2,10 +2,12 @@ package cn.netdiscovery.core.domain.bean;
 
 import cn.netdiscovery.core.domain.Request;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 /**
  * Created by tony on 2019-05-13.
  */
+@EqualsAndHashCode(callSuper=false)
 @Data
 public class SpiderJobBean extends BaseJobBean {
 
