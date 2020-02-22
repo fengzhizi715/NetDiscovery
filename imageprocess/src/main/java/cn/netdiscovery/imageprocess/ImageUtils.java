7ae5193438129a3e401fd9a8c6e63be4d5af4c70
fengzhizi715 <fengzhizi715@126.com>
2020-02-22 23:55:17 +0800

    add ImagePreprocessingService

---
+++ imageprocess/src/main/java/cn/netdiscovery/imageprocess/ImageUtils.java
@@ -0,0 +1,37 @@
+package cn.netdiscovery.imageprocess;
+
+import java.io.*;
+
+/**
+ * @FileName: cn.netdiscovery.imageprocess.ImageUtils
+ * @author: Tony Shen
+ * @date: 2020-02-22 23:49
+ * @version: V1.0 <描述当前版本功能>
+ */
+public class ImageUtils {
+
+    public static byte[] file2Byte (File file) {
+        byte[] buffer = null;
+        FileInputStream fis;
+        try {
+            ByteArrayOutputStream bos = new ByteArrayOutputStream();
+            fis = new FileInputStream(file);
+            byte[] b = new byte[1024];
+            int n;
+            while ((n = fis.read(b)) != -1) {
+                bos.write(b, 0, n);
+            }
+            fis.close();
+            bos.close();
+            buffer = bos.toByteArray();
+            if(file.exists()) {
+                file.delete();
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }catch (IOException e) {
+            e.printStackTrace();
+        }
+        return buffer;
+    }
+}
