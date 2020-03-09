e3204b3af563317393d6cb7712a7aea475bc935f
fengzhizi715 <fengzhizi715@126.com>
2020-03-09 19:26:47 +0800

    add RPCVerticle

---
+++ core/engine/src/main/java/cn/netdiscovery/core/verticle/RPCVerticle.java
@@ -0,0 +1,13 @@
+package cn.netdiscovery.core.verticle;
+
+import io.vertx.core.*;
+
+/**
+ * @FileName: cn.netdiscovery.core.verticle.RPCVerticle
+ * @author: Tony Shen
+ * @date: 2020-03-09 19:17
+ * @version: V1.0 <描述当前版本功能>
+ */
+public class RPCVerticle extends AbstractVerticle {
+
+}
